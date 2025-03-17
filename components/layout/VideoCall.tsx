6d0bd4120a048b40b16ad41166d5542b7af6a6f8
sumit-saurabh98 <sumitsaurabh112@gamil.com>
2025-03-17 19:56:53 +0530

    functionality for togglr button

--- components/layout/VideoCall.tsx
+++ components/layout/VideoCall.tsx
@@ -1,17 +1,73 @@
-"use client"
+"use client";
 
-import { useSocket } from "@/context/SocketContext"
-import VideoContainer from "./VideoContainer"
+import { useSocket } from "@/context/SocketContext";
+import VideoContainer from "./VideoContainer";
+import { MdMic, MdMicOff, MdVideocamOff, MdVideocam } from "react-icons/md";
+import { useCallback, useEffect, useState } from "react";
 
 const VideoCall = () => {
+  const [isMicOn, setIsMicOn] = useState(true);
+  const [isVidOn, setIsVidOn] = useState(true);
+  const { localStream } = useSocket();
 
-    const {localStream} = useSocket()
+  useEffect(() => {
+    if (localStream) {
+      const audioTrack = localStream.getAudioTracks()[0];
+      if (audioTrack) setIsMicOn(audioTrack.enabled);
+
+      const videoTrack = localStream.getVideoTracks()[0];
+      if (videoTrack) setIsVidOn(videoTrack.enabled);
+    }
+  }, [localStream]);
+
+  const toggleCamera = useCallback(() => {
+    if (localStream) {
+      const videoTrack = localStream.getVideoTracks()[0];
+      if (videoTrack) {
+        videoTrack.enabled = !videoTrack.enabled;
+        setIsVidOn(videoTrack.enabled);
+      }
+    }
+  }, [localStream]);
+
+  const toggleMic = useCallback(() => {
+    if (localStream) {
+      const audioTrack = localStream.getAudioTracks()[0];
+      if (audioTrack) {
+        audioTrack.enabled = !audioTrack.enabled;
+        setIsMicOn(audioTrack.enabled);
+      }
+    }
+  }, [localStream]);
 
   return (
     <div>
-      {localStream && <VideoContainer stream={localStream} isLocalStream={true} isOnCall={false}/>}
-    </div>
-  )
-}
+      <div>
+        {localStream && (
+          <VideoContainer
+            stream={localStream}
+            isLocalStream={true}
+            isOnCall={false}
+          />
+        )}
+      </div>
 
-export default VideoCall
+      <div className="mt-8 flex items-center justify-center">
+        <button onClick={toggleMic}>
+          {isMicOn ? <MdMic size={28} /> : <MdMicOff size={28} />}
+        </button>
+        <button
+          className="px-4 py-2 bg-rose-500 text-white rounded mx-4"
+          onClick={() => {}}
+        >
+          End Call
+        </button>
+        <button onClick={toggleCamera}>
+          {isVidOn ? <MdVideocam size={28} /> : <MdVideocamOff size={28} />}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default VideoCall;
